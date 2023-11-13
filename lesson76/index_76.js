3704a20bc2f7913f2ba87eed796a415b55012f5c
KoggdaBro <136738096+KoggdaBro@users.noreply.github.com>
2023-11-13 22:08:16 +0300

    Add files via upload

---
+++ lesson76/index_76.js
@@ -0,0 +1,10 @@
+// let - позволяет объявлять переменные которые в любой момент могут изменится.
+// const - чёкто указывает какое значение будет лежать в данной переменной.Изменить это значение или присвоить чуть позже не выйдет.Использовать const везде где возможно.
+const lastName = 'ivanov'
+const firstName = 'ivan'
+const patronymic = 'ivanovich'
+const age = '100'
+const nationality = 'russian'
+const city = 'Saint Petersburg'
+// let,const,var(один из способов объявить переменную) firstName(название переменной) = (оператор присваивания) 'John'(Значение в одинарных кавычках использум только их)
+console.log(lastName, firstName, patronymic, age, 'y.o', nationality, city)
